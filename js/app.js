09d6d0838b245dacf53e75bc3d2913e4a82428f7
oceane <oceane.gatien.cef@hotmail.fr>
2017-03-09 16:13:43 +0100

    correction avec formateur

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function addition(a, b) {
+	return a +b;
+	}
+
+console.log(addition(44, 6))
+
+
+
+function division(a, b) {
+	return a /b;
+}
+if (b> 0) {
+	console.log(a / b);
+}else {
+	console.log("Can't divide by 0");
+}
+
+console.log(division(33, 6));
